3c32fccb39e161ed310c9e42a129c8783de68ac2
Neolabs-sftw <neolabs.sftw@gmail.com>
2026-03-09 15:24:20 -0300

    Feat:

--- src/index.ts
+++ src/index.ts
@@ -76,80 +76,10 @@ async function startServer() {
         { passageiroId: "3", statusPresenca: "Agendado" },
       ],
     };
-    const dadosExemploVoucher2 = {
-      origem: "Sede Central",
-      destino: "Filial Norte",
-      dataHoraProgramado: new Date(),
-      natureza: "Extra",
-      tipoCorrida: "Saida",
-      status: "Aberto",
-
-      // ADICIONA ESTES CAMPOS (e outros que sejam obrigatórios no teu schema)
-      valorViagem: 90.0,
-      valorViagemRepasse: 70.0,
-      valorDeslocamento: 0,
-      valorDeslocamentoRepasse: 0,
-      valorHoraParada: 0,
-      valorHoraParadaRepasse: 0,
-      valorPedagio: 0,
-      valorEstacionamento: 0,
-
-      // IDs de exemplo (certifica-te que estes IDs existem no teu DB)
-      empresaClienteId: "1",
-      unidadeClienteId: "1",
-      motoristaId: "1",
-      solicitanteId: "1",
-      adminUsuarioId: "1",
-      operadoraId: "1",
-      carroId: "1",
-
-      passageiros: [
-        { passageiroId: "3", statusPresenca: "Agendado" },
-        { passageiroId: "4", statusPresenca: "Agendado" },
-        { passageiroId: "5", statusPresenca: "Agendado" },
-      ],
-    };
-    const dadosExemploVoucher3 = {
-      origem: "Sede Central",
-      destino: "Filial Norte",
-      dataHoraProgramado: new Date(),
-      natureza: "Turno",
-      tipoCorrida: "Entrada",
-      status: "Aberto",
-
-      // ADICIONA ESTES CAMPOS (e outros que sejam obrigatórios no teu schema)
-      valorViagem: 150.0,
-      valorViagemRepasse: 140.0,
-      valorDeslocamento: 0,
-      valorDeslocamentoRepasse: 0,
-      valorHoraParada: 0,
-      valorHoraParadaRepasse: 0,
-      valorPedagio: 0,
-      valorEstacionamento: 0,
-
-      // IDs de exemplo (certifica-te que estes IDs existem no teu DB)
-      empresaClienteId: "1",
-      unidadeClienteId: "1",
-      motoristaId: "1",
-      solicitanteId: "1",
-      adminUsuarioId: "1",
-      operadoraId: "1",
-      carroId: "1",
-
-      passageiros: [
-        { passageiroId: "6", statusPresenca: "Agendado" },
-        { passageiroId: "7", statusPresenca: "Agendado" },
-        { passageiroId: "20", statusPresenca: "Agendado" },
-      ],
-    };
     try {
       console.log("Executando Cron", new Date().toLocaleTimeString());
       const novoVoucher = await executarCriacaoVoucher(dadosExemploVoucher);
-      const novoVoucher2 = await executarCriacaoVoucher(dadosExemploVoucher2);
-      const novoVoucher3 = await executarCriacaoVoucher(dadosExemploVoucher3);
-      console.log(
-        `Voucher criado com sucesso! ID: ${novoVoucher.id}, ${novoVoucher2.id}, ${novoVoucher3.id} `,
-      );
+      console.log(`Voucher criado com sucesso! ID: ${novoVoucher.id} `);
 
       res
         .status(200)
